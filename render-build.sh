85eb6b4b3386e2ea42e2c81672481dd7084162b3
rodwalker3 <ejwalker3@icloud.com>
2026-03-19 11:01:59 -0400

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 
 # Install Playwright browsers (CORRECT WAY for Render)
-python -m playwright install --with-deps chromium
+python -m playwright install chromium
